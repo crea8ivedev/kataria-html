a419f41c0dda3c032fac7304320194b47fe88cca
creative dev <crea8ivedev@gmail.com>
2025-11-17 16:00:14 +0530

    Upadte index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,28 @@
-const swiper = new Swiper('.swiper', {
-    direction: 'vertical',   // for ticker-like announcement bar
-    loop: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-  });
+// const swiper = new Swiper('.swiper', {
+//     direction: 'vertical',   // for ticker-like announcement bar
+//     loop: true,
+//     autoplay: {
+//       delay: 2500,
+//       disableOnInteraction: false,
+//     },
+//   });
+
+
+document.getElementById("menuToggle").onclick = function () {
+    document.getElementById("mobileMenu").classList.toggle("hidden");
+};
+
+if (document.querySelector('.announcement')) {
+    const announcement = document.querySelector('.announcement');
+
+    new Swiper('.announcement-bar', {
+        slidesPerView: 'auto',
+        spaceBetween: 0,
+        loop: true,
+        autoplay: {
+            delay: 0,
+        },
+        speed: 8000,
+        allowTouchMove: false,
+    });
+}
