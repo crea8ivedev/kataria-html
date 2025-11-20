14a25d0a620d9745a8a11cb71a2c2d04821c6d1d
creative dev <crea8ivedev@gmail.com>
2025-11-20 18:13:55 +0530

    Update code

--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -1,19 +1,20 @@
 const initMObileMenu = () => {
 
-const btn = document.getElementById("mobileMenuBtn");
-    const menu = document.getElementById("mobileMenu");
-
-    if (btn && menu) {
-      btn.addEventListener("click", function () {
-        menu.classList.toggle("hidden");
-      });
-
-      // Optional: hide mobile menu when resizing to desktop
-      window.addEventListener("resize", function () {
-        if (window.innerWidth >= 1024) {
-          menu.classList.add("hidden");
-        }
-      });
-    }
+  const btn = document.getElementById("mobileMenuBtn");
+  const menu = document.getElementById("mobileMenu");
+  
+  if (btn && menu) {
+    btn.addEventListener("click", function () {
+      menu.classList.toggle("hidden");
+    });
+  
+    // when going back to desktop, always hide mobile menu
+    window.addEventListener("resize", function () {
+      if (window.innerWidth >= 1199) {
+        menu.classList.add("hidden");
+      }
+    });
+  }
+  
 }
 export default initMObileMenu;
